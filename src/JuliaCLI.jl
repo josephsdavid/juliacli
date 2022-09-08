fb0a819f332aa7dae0d56406b067a6faa594886f
josephsdavid <josephsd@smu.edu>
2022-09-07 23:27:22 -0500

    ignore startup.jl

--- src/JuliaCLI.jl
+++ src/JuliaCLI.jl
@@ -1,11 +1,13 @@
 module JuliaCLI
-using Pkg: depots1
-using LanguageServer, SymbolServer
+
 using Comonicon
+using LanguageServer, SymbolServer
+using Pkg: depots1
 
 # shamelessly borrowed from https://github.com/ExpandingMan/LSPNeovim.jl, which is no longer maintained
 depotpath() = get(ENV, "JULIA_DEPOT_PATH", depots1())
 _defaultenvpath() = dirname(Base.load_path_expand("@v#.#"))
+_defaultenvpath(x) = x
 
 """
     hasmanifest(dir)
@@ -13,7 +15,8 @@ _defaultenvpath() = dirname(Base.load_path_expand("@v#.#"))
 Checks whether there is a valid `Manifest.toml` in directory `dir`.  If no argument is given, it
 will check for the `Manifest.toml` in the `LSPNeovim` environment.
 """
-hasmanifest(dir::AbstractString) = any(map(isfile, joinpath.(dir, ["Manifest.toml", "Project.toml"])))
+hasmanifest(dir::AbstractString) =
+    any(map(isfile, joinpath.(dir, ["Manifest.toml", "Project.toml"])))
 
 _juliaproject() = get(ENV, "JULIA_PROJECT", nothing)
 _juliaprojectbase() = Base.current_project()
@@ -40,7 +43,7 @@ TODO: Update me
 
 The first of these to contain a `Manifest.toml` will be the environment used for LanguageServer.
 """
-function envpath(dirs=[pwd(), joinpath(pwd(), ".."), _defaultenvpath()])
+function envpath(dirs = [pwd(), joinpath(pwd(), ".."), _defaultenvpath()])
     dirs = filter(hasmanifest, dirs)
     isempty(dirs) ? resolve_julia_project() : first(dirs)
 end
@@ -50,11 +53,11 @@ Run the `LanguageServerInstance`.  This will also activate the `LSPNeovim` envir
 By default, this will attempt to determine an appropriate environment, see `envpath`.
 """
 function runserver(
-    env=envpath(),
-    depot=depotpath();
-    input::IO=stdin,
-    output::IO=stdout,
-    download=false
+    env = envpath(),
+    depot = depotpath();
+    input::IO = stdin,
+    output::IO = stdout,
+    download = false,
 )
     @info("Initializing Language Server", pwd(), env, depot)
     s = LanguageServer.LanguageServerInstance(
@@ -78,7 +81,7 @@ end
 --download, -d: Whether or not to download symbolserver cache
 """
 
-@cast function server(; download::Bool=false)
+@cast function server(; download::Bool = false)
     return runserver(; download)
 end
 
@@ -86,7 +89,7 @@ end
 parse_args(v::Vararg{Any}) = reduce((x, y) -> `$(x), $(y)`, [v...])
 parse_args(v::Vararg{Any,1}) = "\"$(v...)\""
 
-@cast function pkg(cmd::AbstractString, args...;)
+@cast function pkg(cmd::AbstractString, args...; project=envpath(), global_env::Bool=false)
     # TODO: Figure out kwds
     # args, kwds = map([[args...], [kwds...]]) do param
     #     reduce((x, y) -> `$(x), $(y)`, param)
@@ -96,7 +99,11 @@ parse_args(v::Vararg{Any,1}) = "\"$(v...)\""
     else
         pkg_cmd = "using Pkg; Pkg.$(cmd)()"
     end
-    fn = `julia --project=$(envpath()) -e $(pkg_cmd)`
+    if !global_env
+        fn = `julia --history-file=no --startup-file=no --project=$(project) -e $(pkg_cmd)`
+    else
+        fn = `julia --history-file=no --startup-file=no --project=$(Base.load_path_expand("@v#.#")) -e $(pkg_cmd)`
+    end
     @info "running" fn
     run(fn)
 end
